e0cc3f5334196480d93fb106c2f6e47d4df60467
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-04-25 18:51:20 +0200

    Add prop-types definition and docs for Friends

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,6 +1,14 @@
-import css from "./FriendList.module.css"
+import PropTypes from "prop-types";
 import { FriendListItem } from "components";
+import css from "./FriendList.module.css"
 
+/**
+ * A FriendList React component which contains the list of the user's friends.
+ * 
+ * @param {Object[]} props.friends An array of friend objects of any length.
+ * 
+ * @returns FriendList React component.
+ */
 export const FriendList = ({friends=[]}) => (
   <section className={css.friends}>
     <ul className={css["friend-list"]}>
@@ -8,3 +16,7 @@ export const FriendList = ({friends=[]}) => (
     </ul>
   </section>
 );
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,10 +1,20 @@
-import css from "./FriendListItem.module.css";
+import PropTypes from "prop-types";
 import noAvatar from "img/no-image.png";
+import css from "./FriendListItem.module.css";
 
+/**
+ * A Friend React component which displays information about a single user's friend.
+ * 
+ * @param {string} [props.avatar=noAvatar] Link to the avatar. If no avatar - avatar replaced with default image.
+ * @param {string} props.name Friend's name.
+ * @param {boolean} [props.isOnline=false] Boolean indicating the status of a friend, online or offline.
+ * 
+ * @returns Friend React component.
+ */
 export const FriendListItem = ({
   avatar = noAvatar,
   name,
-  isOnline }) => (
+  isOnline = false }) => (
     <li className={css.item}>
       {isOnline
         ? (<span className={`${css.status} ${css["is-online"]}`}></span>)
@@ -12,4 +22,10 @@ export const FriendListItem = ({
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
-  );
+);
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool,
+};
