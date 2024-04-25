f61f62b92b7aa561c3c8d2c5a00d004517f82f99
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-04-25 19:02:44 +0200

    Add prop-types definition and docs for TransactionHistory

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,5 +1,17 @@
+import PropTypes from "prop-types";
 import css from "./TransactionHistory.module.css";
 
+/**
+ * A React component of the transaction history in the personal profile of the Internet Bank.
+ * 
+ * @param {object[]} props.items An array of single transaction objects.
+ * @param {string} props.item.id Unique transaction identifier.
+ * @param {string} props.item.type Transaction type.
+ * @param {string} props.item.amount Transaction amount.
+ * @param {string} props.item.currency Currency type.
+ * 
+ * @returns React component.
+ */
 export const TransactionHistory = ({ items }) => (
   <table className={css["transaction-history"]}>
     <thead>
@@ -21,3 +33,16 @@ export const TransactionHistory = ({ items }) => (
     </tbody>
   </table>
 );
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact(
+      {
+        id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+        currency: PropTypes.string.isRequired,
+      }
+    )
+  ).isRequired,
+};
